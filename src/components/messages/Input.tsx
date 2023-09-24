213f45c4c66489b55d2dc8ca5d7a0fd02067114c
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-09-24 16:48:44 +0200

    Handle input change

--- src/components/messages/Input.tsx
+++ src/components/messages/Input.tsx
@@ -1,8 +1,50 @@
+import { Message } from 'interfaces/Message'
 import styled from 'styled-components'
 
-export function InputMessage() {
-  return <InputStyle placeholder="Message John Doe" />
+interface Props {
+  message: Message
+  onSend: () => void
+  onChange: (e: any) => void
 }
+
+export function InputMessage({ message, onSend, onChange }: Props) {
+  return (
+    <InputContainer>
+      <InputStyle
+        onChange={(e) => onChange(e)}
+        placeholder="Message John Doe"
+      />
+      <SendButton onClick={onSend}>Send</SendButton>
+    </InputContainer>
+  )
+}
+
+const InputContainer = styled.div`
+  width: calc(100% - 220px);
+  display: flex;
+  height: fit-content;
+  position: fixed;
+  bottom: 10px;
+  align-items: center;
+  justify-content: space-evenly;
+`
+
+const SendButton = styled.button`
+  background-color: transparent;
+  padding: 5px;
+  color: black;
+  width: fit-content;
+  height: fit-content;
+  position: fixed;
+  right: 40px;
+  border: 1px solid black;
+  &:hover {
+    cursor: pointer;
+  }
+  &:active {
+    background-color: lightgray;
+  }
+`
 
 const InputStyle = styled.input`
   padding: 1rem;
@@ -11,9 +53,7 @@ const InputStyle = styled.input`
   font-size: 1rem;
   outline: none;
   box-sizing: border-box;
-  position: fixed;
-  bottom: 10px;
-  width: calc(100% - 220px);
+  width: 100%;
   &:hover,
   &:active,
   &:focus {
