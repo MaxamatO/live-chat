95cf992f8af9fd4fe5dda15a06e251600b3403a8
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-09-24 16:49:54 +0200

    Handle sockets connection

--- src/components/messages/MainContent.tsx
+++ src/components/messages/MainContent.tsx
@@ -2,13 +2,54 @@ import styled from 'styled-components'
 import { MainContentHeader } from './MainContentHeader'
 import { MessageBox } from './MessageBox'
 import { InputMessage } from './Input'
+import { useEffect, useState } from 'react'
+import useWebSocket from 'react-use-websocket'
+import { Message } from 'interfaces/Message'
 
 export function MainContent() {
+  const [message, setMessage] = useState('')
+  const [socketUrl] = useState('ws://127.0.0.1:1234')
+  const [messageHistory, setMessageHistory] = useState<Message[]>([])
+  const { sendMessage, lastMessage } = useWebSocket(socketUrl, {
+    onOpen() {
+      sendMessage('This is client!')
+    },
+    shouldReconnect() {
+      return true
+    },
+  })
+  function onChange(e: any) {
+    setMessage(e.target.value)
+  }
+
+  function onSendMessage() {
+    console.log('Im sending')
+
+    sendMessage(message)
+  }
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      console.log(lastMessage.data)
+
+      const receivedMessage = JSON.parse(lastMessage.data) as Message
+      setMessageHistory((prev) => [...prev, receivedMessage])
+    }
+  }, [lastMessage])
+
   return (
     <Container>
       <MainContentHeader></MainContentHeader>
-      <MessageBox></MessageBox>
-      <InputMessage></InputMessage>
+      <MessageBox messageHistory={messageHistory}></MessageBox>
+      <InputMessage
+        message={{
+          user: { username: 'Leon' },
+          date: '19.09.2023',
+          message: 'Just a message',
+        }}
+        onChange={(e) => onChange(e)}
+        onSend={onSendMessage}
+      ></InputMessage>
     </Container>
   )
 }
