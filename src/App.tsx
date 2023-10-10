04f25cc7cc747010df2240c00bcdab221f30d8af
maks <pierchala.maks@gmail.com>
2023-10-10 23:46:06 +0200

    Add auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Layout } from 'components/layout/Layout'
+import { Route, Routes } from 'react-router-dom'
+import { LoginPage } from '../src/pages/LoginPage'
+import { RegisterPage } from '../src/pages/RegisterPage'
 
 function App() {
   return (
     <>
-      <Layout />
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </>
   )
 }
